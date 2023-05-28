827ca045f120ec9131598afb29dcdb251c76d89d
Andrey <Andreas42R@yandex.ru>
2023-05-28 08:38:14 +0700

    rateLimiter to midllewares

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const {
   celebrate, Joi, errors, Segments,
 } = require('celebrate');
 
+const rateLimiter = require('./middlewares/rateLimiter');
 const auth = require('./middlewares/auth');
 
 const errorHandler = require('./utils/errorHandler');
@@ -32,15 +32,8 @@ app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-
 // Apply the rate limiting middleware to all requests
-app.use(limiter);
+app.use(rateLimiter);
 app.use(helmet());
 
 app.use(requestLogger);
